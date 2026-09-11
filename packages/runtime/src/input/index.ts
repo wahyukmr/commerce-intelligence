997a7adc6e1706e6f2f021053d67f117c62b8eb4
wahyukmr <maryu9229@gmail.com>
2026-09-11 14:50:35 +0700

    feat(handler): implement createDashboardWebhookHandler for webhook processing and response mapping

--- packages/runtime/src/input/index.ts
+++ packages/runtime/src/input/index.ts
@@ -1,2 +1,5 @@
 export * from "./event-envelope-validation.js";
 export * from "./event-ingestion-handler.js";
+export * from "./http-ingestion-composition.js";
+export * from "./http-webhook-adapter.js";
+export * from "./ingestion-adapter.js";
